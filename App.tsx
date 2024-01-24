ec1aa31dd83a29e25a9a9978c8f183b9f3210dc6
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-01-24 14:54:44 +0700

    Refactor code

--- App.tsx
+++ App.tsx
@@ -7,23 +7,7 @@
 
 import React from 'react';
 // eslint-disable-next-line prettier/prettier
-import { Image, StyleSheet, View } from 'react-native';
-
-const SplashScreen = (): React.JSX.Element => {
-  return (
-    <View style={styles.splashScreen}>
-      <View style={styles.logoContainer}>
-        <Image
-          source={require('./assets/Icon/iconWelcome.png')}
-          style={{
-            height: 32,
-            width: 32,
-          }}
-        />
-      </View>
-    </View>
-  );
-};
+import SplashScreen from './src/screens/SplashScreen';
 
 function App(): React.JSX.Element {
   return (
@@ -32,22 +16,5 @@ function App(): React.JSX.Element {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  splashScreen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#40BFFF',
-  },
-  logoContainer: {
-    backgroundColor: '#FFF',
-    height: 72,
-    width: 72,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-  },
-});
 
 export default App;
---
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { Image, StyleSheet, View } from 'react-native';
+
+function SplashScreen(): React.JSX.Element {
+    return (
+        <View style={styles.splashScreen}>
+            <View style={styles.logoContainer}>
+                <Image
+                    source={require('../../../assets/Icon/iconWelcome.png')}
+                    style={{
+                        height: 32,
+                        width: 32,
+                    }}
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    splashScreen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#40BFFF',
+    },
+    logoContainer: {
+        backgroundColor: '#FFF',
+        height: 72,
+        width: 72,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+    },
+});
+
+export default SplashScreen;

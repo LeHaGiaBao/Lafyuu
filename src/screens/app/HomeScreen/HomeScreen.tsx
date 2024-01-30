1742bd753c97b6b75effa26db4c62b418f320390
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-01-30 11:50:57 +0700

    Add home header

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeHeader from '../../../containers/app/HomeScreen/HomeHeader';
 
 function HomeScreen() {
     return (
-        <View>
+        <View style={styles.homeScreen}>
+            <View style={styles.homeHeader}>
+                <HomeHeader />
+            </View>
+            <View style={styles.homeScreenContainer}>
+
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    homeScreen: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 60,
+    },
+    homeHeader: {
+        width: '100%',
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingBottom: 16,
+        borderBottomColor: '#EBF0FF',
+        borderTopColor: '#FFF',
+        borderLeftColor: '#FFF',
+        borderRightColor: '#FFF',
+        borderWidth: 1,
+    },
+    homeScreenContainer: {
+        marginLeft: 16,
+        marginRight: 16,
+    }
+})
 
 export default HomeScreen;

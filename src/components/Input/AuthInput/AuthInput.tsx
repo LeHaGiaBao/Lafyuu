617ac148551e71aa98b236fb97fc6818eb1e36c2
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-02-29 16:35:31 +0700

    Refactor code

---
+++ src/components/Input/AuthInput/AuthInput.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, TextInput, View } from 'react-native';
+import { FontFamily, FontSize } from '../../../constants/fonts';
+import { NeutralColor, PrimaryColor } from '../../../constants/colors';
+import { lockIcon, mailIcon, userIcon } from '../../../assets';
+
+interface AuthInputProps {
+    placeholder: string
+    icon: string,
+    error: boolean,
+    security: boolean
+}
+
+function AuthInput({ placeholder, icon, error, security }: AuthInputProps) {
+    const [focus, setFocus] = useState(false)
+    let borderStyles
+    let iconName
+
+    if (!error) {
+        switch (focus) {
+            case true:
+                borderStyles = styles.focusBorder
+                switch (icon) {
+                    case 'email':
+                        iconName = mailIcon.mailIconFocus
+                        break
+                    case 'password':
+                        iconName = lockIcon.lockIconFocus
+                        break
+                    case 'profile':
+                        iconName = userIcon.userIconFocus
+                        break
+                }
+                break
+            case false:
+                borderStyles = styles.defaultBorder
+                switch (icon) {
+                    case 'email':
+                        iconName = mailIcon.mailIcon
+                        break
+                    case 'password':
+                        iconName = lockIcon.lockIcon
+                        break
+                    case 'profile':
+                        iconName = userIcon.userIcon
+                        break
+                }
+                break
+        }
+    } else {
+        borderStyles = styles.errorBorder
+        switch (icon) {
+            case 'email':
+                iconName = mailIcon.mailIconError
+                break
+            case 'password':
+                iconName = lockIcon.lockIconError
+                break
+        }
+    }
+
+    return (
+        <View style={[styles.inputContainer, borderStyles]}>
+            <Image source={iconName} style={styles.iconStyle} />
+            <TextInput
+                placeholder={placeholder}
+                autoCapitalize="none"
+                style={styles.inputBox}
+                secureTextEntry={security}
+                onFocus={() => setFocus(true)}
+                onBlur={() => setFocus(false)}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '100%',
+        height: 48,
+        paddingLeft: 16,
+        paddingRight: 16,
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    defaultBorder: {
+        borderColor: NeutralColor.LightColor,
+    },
+    focusBorder: {
+        borderColor: PrimaryColor.BlueColor
+    },
+    errorBorder: {
+        borderColor: PrimaryColor.RedColor
+    },
+    iconStyle: {
+        marginRight: 10,
+    },
+    inputBox: {
+        width: '90%',
+        fontFamily: FontFamily.FontRegular,
+        fontSize: FontSize.FontSize12,
+        color: NeutralColor.GreyColor,
+    },
+})
+
+export default AuthInput;
--- src/containers/auth/LoginForm/LoginForm.tsx
+++ src/containers/auth/LoginForm/LoginForm.tsx
@@ -1,39 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import Feather from 'react-native-vector-icons/Feather'
+import { StyleSheet, Text, View } from 'react-native';
 import ButtonBlue from '../../../components/Button/ButtonBlue';
+import AuthInput from '../../../components/Input/AuthInput';
+import { FontFamily, FontSize } from '../../../constants/fonts';
+import { PrimaryColor } from '../../../constants/colors';
 
 function LoginForm({ navigation }: any) {
-    const [emailFocus, setEmailFocus] = useState(false)
-    const [passwordFocus, setPasswordFocus] = useState(false)
+    const [authError, setAuthError] = useState(false)
 
     return (
         <>
             <View style={styles.loginForm}>
-                <View>
-                    <View style={emailFocus ? styles.inputContainerFocus : styles.inputContainer}>
-                        <Feather name="mail" size={24} style={emailFocus ? styles.iconStylesFocus : styles.iconStyles} />
-                        <TextInput
-                            placeholder='Your email'
-                            autoCapitalize="none"
-                            style={styles.inputBox}
-                            onFocus={() => setEmailFocus(true)}
-                            onBlur={() => setEmailFocus(false)}
-                        />
-                    </View>
-                    {/* <Text style={styles.wrongText}> Oops! Your Email Is Not Correct </Text> */}
-                    <View style={passwordFocus ? styles.inputPasswordFocus : styles.inputPassword}>
-                        <Feather name="lock" size={24} style={passwordFocus ? styles.iconStylesFocus : styles.iconStyles} />
-                        <TextInput
-                            secureTextEntry
-                            placeholder='Password'
-                            autoCapitalize="none"
-                            style={styles.inputBox}
-                            onFocus={() => setPasswordFocus(true)}
-                            onBlur={() => setPasswordFocus(false)}
-                        />
-                    </View>
-                    {/* <Text style={styles.wrongText}> Oops! Your Password Is Not Correct </Text> */}
+                <View style={styles.inputForm}>
+                    <AuthInput
+                        placeholder='Your email'
+                        icon='email'
+                        error={authError}
+                        security={false}
+                    />
+                    {authError && <Text style={styles.wrongText}>Oops! Your Email Is Not Correct</Text>}
+                    <AuthInput
+                        placeholder='Password'
+                        icon='password'
+                        error={authError}
+                        security={true}
+                    />
+                    {authError && <Text style={styles.wrongText}>Oops! Your Password Is Not Correct</Text>}
                 </View>
                 <ButtonBlue title='Sign In' onPress={() => navigation.navigate('AppScreen')} />
             </View>
@@ -49,110 +41,15 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         gap: 16,
     },
-    inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: 48,
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingLeft: 16,
-        paddingRight: 16,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#EBF0FF',
-    },
-    inputContainerFocus: {
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: 48,
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingLeft: 16,
-        paddingRight: 16,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#40BFFF',
-    },
-    iconStyles: {
-        color: '#9098B1',
-        marginRight: 10,
-    },
-    iconStylesFocus: {
-        color: '#40BFFF',
-        marginRight: 10,
-    },
-    inputBox: {
-        paddingTop: 3,
-        fontFamily: 'Poppins-Bold',
-        fontSize: 12,
-        color: '#9098B1',
-    },
-    inputPassword: {
-        marginTop: 8,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: 50,
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingLeft: 16,
-        paddingRight: 16,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#EBF0FF',
-    },
-    inputPasswordFocus: {
-        marginTop: 8,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: 48,
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingLeft: 16,
-        paddingRight: 16,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#40BFFF',
+    inputForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 8,
     },
     wrongText: {
-        marginTop: 8,
-        fontFamily: 'Poppins-Bold',
-        fontSize: 12,
-        color: '#FB7181',
-    },
-    inputContainerEmailWrong: {
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: 48,
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingLeft: 16,
-        paddingRight: 16,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#FB7181',
-    },
-    inputContainerPasswordWrong: {
-        marginTop: 8,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: 48,
-        paddingTop: 12,
-        paddingBottom: 12,
-        paddingLeft: 16,
-        paddingRight: 16,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#FB7181',
-    },
-    iconStylesWrong: {
-        color: '#FB7181',
-        marginRight: 10,
+        fontFamily: FontFamily.FontBold,
+        fontSize: FontSize.FontSize12,
+        color: PrimaryColor.RedColor,
     },
 })
 

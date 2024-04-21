e54919320fb8070c2c954a0b11fa2aa8b371c06e
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-04-21 20:58:21 +0700

    update project

---
+++ src/constants/fonts.ts
@@ -0,0 +1,17 @@
+const FontFamily = {
+    FontBold: 'Poppins-Bold',
+    FontRegular: 'Poppins-Regular',
+};
+
+const FontSize = {
+    FontSize32: 32,
+    FontSize24: 24,
+    FontSize20: 20,
+    FontSize18: 18,
+    FontSize16: 16,
+    FontSize14: 14,
+    FontSize12: 12,
+    FontSize10: 10,
+};
+
+export {FontFamily, FontSize};

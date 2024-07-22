b8e8ea9c2e9bf5991e640b98602ae3b0c6d708f2
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-07-22 22:49:52 +0700

    chore: update project

--- src/screens/tabs/Home/index.tsx
+++ src/screens/tabs/Home/index.tsx
@@ -1,15 +1,21 @@
 import React, {memo} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {BackgroundColor} from '@/constants';
+import HomeHeader from './Home.Header';
 
 function HomeScreen() {
-  return <View style={styles.homeContainer} />;
+  return (
+    <View style={styles.homeContainer}>
+      <HomeHeader />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
     backgroundColor: BackgroundColor.WhiteColor,
+    paddingTop: 70,
   },
 });
 

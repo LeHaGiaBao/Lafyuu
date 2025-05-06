4cd887ae875bd647ed097f5e925197c14b75218d
LeHaGiaBao <lehagiabao0205@gmail.com>
2025-05-06 22:56:06 +0700

    feat: add offer screen

--- src/screens/tabs/Offer/index.tsx
+++ src/screens/tabs/Offer/index.tsx
@@ -1,13 +1,25 @@
 import React, {memo} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {LFNavigation} from '@components';
-import {BackgroundColor, Devices} from '@constants';
+import {LFNavigation, LFText} from '@components';
+import {BackgroundColor, Devices, PrimaryColor} from '@constants';
 import translate from '@translations/i18n';
+import OfferSuperFlashsale from './Offer.SuperFlashsale';
+import OfferSuperMega from './Offer.SuperMega';
 
 function OfferScreen() {
   return (
     <View style={styles.offerContainer}>
       <LFNavigation.Header name={translate('navigation:offer')} />
+      <View style={styles.banner}>
+        <LFText.Text
+          typo="H4"
+          color={BackgroundColor.WhiteColor}
+          numberOfLines={2}>
+          Use “MEGSL” Cupon For Get 90% off
+        </LFText.Text>
+      </View>
+      <OfferSuperFlashsale />
+      <OfferSuperMega />
     </View>
   );
 }
@@ -17,6 +29,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: BackgroundColor.WhiteColor,
     paddingTop: Devices.headerTop,
+  },
+  banner: {
+    marginTop: 16,
+    marginHorizontal: 16,
+    padding: 16,
+    backgroundColor: PrimaryColor.BlueColor,
+    borderRadius: 5,
   },
 });
 

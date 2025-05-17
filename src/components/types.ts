8b0c6b2e08661c1279dd174171902841a119ec1c
LeHaGiaBao <lehagiabao0205@gmail.com>
2025-05-17 14:58:49 +0700

    feat: add cart screen

--- src/components/types.ts
+++ src/components/types.ts
@@ -171,6 +171,16 @@ export interface LFProductCardProps
   isFavorite?: boolean;
 }
 
+export interface LFHorizontalCardProps
+  extends Pick<
+      LFProductCardProps,
+      'id' | 'image' | 'name' | 'price' | 'isFavorite'
+    >,
+    PropsWithChildren {
+  numOfProduct?: number;
+  type: 'Cart' | 'Order';
+}
+
 export interface LFProductDetailBannerProps {
   id: string;
   image: ImageSourcePropType;
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
 export * from './account';
+export * from './cart';
 export * from './category';
 export * from './home';
 export * from './notification';
